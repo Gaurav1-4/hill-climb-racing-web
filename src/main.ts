f4dd6930fcc10a13af67ae9ebf9ae2b47c66a03c
Gaurav Goyal <gaurav25212@iiitd.ac.in>
2026-06-11 20:23:46 +0530

    Remove rotate message, enforce strict landscape aspect ratio via FIT

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ import { GameOverScene } from './scenes/GameOverScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: 1024,
+  height: 576,
   parent: 'app',
   scale: {
-    mode: Phaser.Scale.RESIZE,
+    mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   physics: {
